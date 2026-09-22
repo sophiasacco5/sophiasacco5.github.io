fa43634af3ceaf51ef9ead7b5e35cc0e4538d65e
sophiasacco5 <sophia.sacco5@gmail.com>
2026-09-22 13:59:24 -0400

    started java script for damage

--- day7/script.js
+++ day7/script.js
@@ -1,3 +1,12 @@
 let playerHealth = 100;
 const damageAmount = 30;
 
+function takeDamage() {
+    playerHealth = playerHealth - damangeAmount;
+
+    const healthText = document.getElementById("health-display");
+    const statusText = document.getElementById("status-message");
+
+    health-text.innerText = playerHealth;
+    
+}
